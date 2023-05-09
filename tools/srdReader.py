ff9f933f9aecf352a53b42f416103c451c1f41b7
Aaron <aaron4uk@gmail.com>
2023-05-08 20:27:30 -0500

    add tools

---
+++ tools/srdReader.py
@@ -0,0 +1,50 @@
+"""Tool for the SRD API's /classes endpoint."""
+
+import json
+from typing import Optional
+from langchain.callbacks.manager import (AsyncCallbackManagerForToolRun)
+from langchain.tools.base import BaseTool
+
+from langchain.vectorstores import Chroma
+from langchain.embeddings import HuggingFaceEmbeddings
+
+class srdReader(BaseTool):
+    """Tool that adds the capability to search D&D 5E SRD."""
+
+    name = "DNDSRD"
+    db: Chroma = None
+    description = (
+    "Used to retrieve information about the rules and content of Dungeons and Dragons. "
+    "The Action Input should be a brief query for the in-context subject matter you want to learn about. "
+    # "For example, you can use an Action Input of \"Bard hit dice\" where Bard is the context and hit dice is the subject matter, this will retrieve an excerpt from the rules book on the Bard class's hit dice value"
+    )
+
+    def __init__(self):
+        super().__init__()
+        embeddings = HuggingFaceEmbeddings()
+        persist_directory = '.chromaDB/srd'
+        chromaDb = Chroma(persist_directory=persist_directory, embedding_function=embeddings)
+
+        self.db = chromaDb
+
+    def _run(self, ruleBookQuery: str) -> str:
+        results = self.db.similarity_search(ruleBookQuery, k=6)
+        output = f"""DND Search Results:"""
+        seen = set()
+        for index, doc in enumerate(results):
+            if len(seen) >= 2:
+                break
+            if doc.page_content not in seen:
+                seen.add(doc.page_content)
+                output += f"""
+[rank {index+1}] {doc.page_content}"""
+
+        return output
+
+    async def _arun(
+        self,
+        query: str,
+        run_manager: Optional[AsyncCallbackManagerForToolRun] = None,
+    ) -> str:
+        """Use the Wikipedia tool asynchronously."""
+        raise NotImplementedError("WikipediaQueryRun does not support async")
