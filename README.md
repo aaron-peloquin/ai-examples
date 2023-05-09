de50e03047849b24a3ec97d3730ac516bb1832f4
Aaron <aaron4uk@gmail.com>
2023-05-08 22:11:12 -0500

    add srd md file

--- README.md
+++ README.md
@@ -2,6 +2,5 @@
 
 ## Setup
 1. `pip install -r requirements.txt`
-2. Create your own `.\data\srd\srd5-1-creative-commons.md` markdown file
-3. `python .\setup\initSRD.py`
-4. `python .\main.py`
+2. `python .\setup\initSRD.py`
+3. `python .\main.py`
--- main.py
+++ main.py
@@ -41,39 +41,39 @@ agent = initialize_agent(
     verbose=True,
 )
 
-dndQuestion = "Please roll 3d6 to determine my Charisma ability score"
-print(Fore.BLUE, f"Humanoid: {dndQuestion}", Style.RESET_ALL)
-agent_reply = agent.run(dndQuestion)
-print(Fore.LIGHTMAGENTA_EX, f"D&D Bot: {agent_reply}")
+# dndQuestion = "Please roll 3d6 to determine my Charisma ability score"
+# print(Fore.BLUE, f"Humanoid: {dndQuestion}", Style.RESET_ALL)
+# agent_reply = agent.run(dndQuestion)
+# print(Fore.LIGHTMAGENTA_EX, f"D&D Bot: {agent_reply}")
 
-dndQuestion = "(In Dungeons and Dragons) What age are Halflings considered mature?"
-print(Fore.BLUE, f"Humanoid: {dndQuestion}", Style.RESET_ALL)
-agent_reply = agent.run(dndQuestion)
-print(Fore.LIGHTMAGENTA_EX, f"D&D Bot: {agent_reply}")
+# dndQuestion = "(In Dungeons and Dragons) What age are Halflings considered mature?"
+# print(Fore.BLUE, f"Humanoid: {dndQuestion}", Style.RESET_ALL)
+# agent_reply = agent.run(dndQuestion)
+# print(Fore.LIGHTMAGENTA_EX, f"D&D Bot: {agent_reply}")
 
-# full_chain = SimpleSequentialChain(
-#     chains=[agent, conversation],
-#     input_key="context",
-#     output_key="context",
-#     verbose=True,
-# )
+full_chain = SimpleSequentialChain(
+    chains=[agent, conversation],
+    input_key="context",
+    output_key="context",
+    verbose=True,
+)
 
-# print(Fore.RED, '==== STARTING ====')
-# print(Style.RESET_ALL)
-# starter_prompt = "(In Dungeons and Dragons) What age are Halflings considered mature?"
-# print(f"== starter_prompt ({starter_prompt}) ==")
-# print(full_chain.run(starter_prompt))
+print(Fore.RED, '==== STARTING ====')
+print(Style.RESET_ALL)
+starter_prompt = "(In Dungeons and Dragons) What age are Halflings considered mature?"
+print(f"== starter_prompt ({starter_prompt}) ==")
+print(full_chain.run(starter_prompt))
 
-# while True:
-#     print(Fore.CYAN)
-#     human_input = input("Human:")
-#     start_time = time.time()
-#     print(Style.RESET_ALL)
+while True:
+    print(Fore.CYAN)
+    human_input = input("Human:")
+    start_time = time.time()
+    print(Style.RESET_ALL)
     
-#     # ai_chat_reply = conversation.run(input=human_input)
-#     # ai_agent_reply = agent.run(human_input)
-#     ai_chat_agent_reply = full_chain.run(human_input)
+    # ai_chat_reply = conversation.run(input=human_input)
+    # ai_agent_reply = agent.run(human_input)
+    ai_chat_agent_reply = full_chain.run(human_input)
 
-#     print(Fore.LIGHTGREEN_EX, ai_chat_agent_reply, Style.RESET_ALL)
+    print(Fore.LIGHTGREEN_EX, ai_chat_agent_reply, Style.RESET_ALL)
     
-#     print(f"== {time.time() - start_time} ==")
+    print(f"== {time.time() - start_time} ==")
