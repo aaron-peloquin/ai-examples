51c1cb73d0031f279519ef14e73646b073db5130
Aaron <aaron4uk@gmail.com>
2023-05-28 11:13:33 -0500

    delete PaLM2 created code files from working dir

--- ai-written-code/helper.js
+++
@@ -1,5 +0,0 @@
-import { numberList } from './constant';
-
-for (let i = 0; i < numberList.length; i++) {
-  console.log(numberList[i]);
-}
--- ai-written-code/main.js
+++
@@ -1,3 +0,0 @@
-import { countFromOneToTen } from './helper';
-
-countFromOneToTen();
